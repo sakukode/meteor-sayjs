189d2a011596311b13224e3d5c2236b3a5c6d42c
RSJMC <rsjmc@rsjmc-desktop.(none)>
2015-04-11 07:58:54 +0700

    bug fixing

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'sakukode:sayjs',
-  version: '0.0.1',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'TTS (text to speech) for node.js. send text from node.js to your speakers.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/sakukode/meteor-sayjs',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: null
@@ -16,7 +16,7 @@ Npm.depends({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('say.js');
+  api.addFiles('say.js', 'server');
 });
 
 /*
